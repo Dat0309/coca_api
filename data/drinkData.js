3b2d3ea064f6573b2336fd3439f819b7800e73f7
dat.trong <anhtheboy011@gmail.com>
2023-10-17 20:12:14 +0700

    update

--- data/drinkData.js
+++ data/drinkData.js
@@ -1,12 +1,76 @@
 const drinks = [
     {
-      first_name: "Food Recipe",
-      last_name: "Admin",
-      phone_number: "0865447141",
-      username: "admin",
-      password: "admin",
-      banking_number: "6352000012",
-    }
+      name: "Trà xanh 0 độ",
+      price: 12000,
+      image: "https://iwater.vn/Image/Picture/tra-xanh-khong-do.jpg",
+      quantity: 20,
+    },
+    {
+      name: "7Up lon hương chanh",
+      price: 12000,
+      image: "https://product.hstatic.net/1000141988/product/nuoc_ngot_7up_huong_chanh_lon_330_ml_63d1cee15d314dc39522d5e83784d4d4.jpg",
+      quantity: 20,
+    },
+    {
+      name: "7Up chai hương chanh",
+      price: 12000,
+      image: "https://salt.tikicdn.com/cache/w1200/ts/product/39/50/19/582c5d68e7a4509fe05c3d46a71b2a84.jpg",
+      quantity: 20,
+    },
+    {
+      name: "Xá xị chương dương",
+      price: 12000,
+      image: "https://cdn.tgdd.vn/Products/Images/2443/85627/bhx/nuoc-ngot-fanta-huong-sa-xi-330ml-201912091405558984.jpg",
+      quantity: 20,
+    },
+    {
+      name: "Cam ép Twister lon",
+      price: 12000,
+      image: "https://tomitamart.vn/public/media//00387.jpg",
+      quantity: 20,
+    },
+    {
+      name: "Coca Cola chai",
+      price: 12000,
+      image: "https://product.hstatic.net/200000356473/product/cocacola-chai-390ml_7214ffae946e4e63826e8f38a45ed5fa.jpg",
+      quantity: 20,
+    },
+    {
+      name: "Pepsi",
+      price: 12000,
+      image: "https://cdn.tgdd.vn/Products/Images/2443/76467/bhx/nuoc-ngot-pepsi-cola-lon-320ml-202303151108006292.jpg",
+      quantity: 20,
+    },
+    {
+      name: "Nước yến",
+      price: 12000,
+      image: "https://iwater.vn/Image/Picture/tra-xanh-khong-do.jpg",
+      quantity: 20,
+    },
+    {
+      name: "Nước sôi để nguội",
+      price: 12000,
+      image: "https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100,s_300x300/https://cdn.tgdd.vn/Products/Images/2563/79249/nuoc-tinh-khiet-dasani-500ml-thumb-600x600.jpg",
+      quantity: 20,
+    },
+    {
+      name: "Xì tin",
+      price: 12000,
+      image: "https://vn-live-01.slatic.net/p/9493e7dc051bcdfdb27f2a72f759e421.jpg",
+      quantity: 20,
+    },
+    {
+      name: "Sữa trái cây Nutriboost",
+      price: 12000,
+      image: "https://csfood.vn/wp-content/uploads/2016/04/Nuoc-Uong-Sua-Trai-Cay-Nutri-Boost-Huong-Dau-Chai-297ml.jpg",
+      quantity: 20,
+    },
+    {
+      name: "Monster",
+      price: 12000,
+      image: "https://89khamthien.com/wp-content/uploads/2018/03/monster-energy-lon-355ml.jpg",
+      quantity: 20,
+    },
   ];
   
   export default drinks;
